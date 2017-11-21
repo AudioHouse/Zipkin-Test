c8eb17429916a433173c989d4e480d2901eff0c4
Austin <austinmiller360@gmail.com>
2017-11-20 21:58:20 -0800

    Initial Commit
    Added source files

---
+++ zipkin-Test/src/main/java/com/zipkin/zipkinTest/ZipkinTestApplication.java
@@ -0,0 +1,12 @@
+package com.zipkin.zipkinTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinTestApplication.class, args);
+	}
+}
